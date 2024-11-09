07b83a77a71907a8b05f711f1386c7db0b9e7292
Harish <harishchaurasia143@gmail.com>
2024-11-09 11:19:15 -0500

    init setup complete

---
+++ src/frontend/landing_page.tsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// const LandingPage: React.FC = () => {
+//   return <div></div>;
+// };
+
+// export default LandingPage;
+
+import React from "react";
+
+const LandingPage: React.FC = () => {
+  return <div></div>;
+};
+
+export default LandingPage;
